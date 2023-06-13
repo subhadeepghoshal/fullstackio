12eed7ee55af4d14ee49dd11f43cd458f1176dad
subhadeepghoshal <subhadeep.ghoshal@gmail.com>
2023-06-13 08:30:13 +0530

    NG-BOOK End Of Chapter 1

---
+++ ng-book/angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input()
+  article!: Article;
+
+  constructor() {
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+}
